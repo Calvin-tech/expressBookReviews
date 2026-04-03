e2574a353db3302ff73f5e5d0a8e33798c482350
Calvin <calvingareth68@gmail.com>
2026-04-03 04:45:35 +0300

    Final submission: All tasks implemented with Promises and Auth logic

--- router/general.js
+++ router/general.js
@@ -4,55 +4,66 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-// Task 10: Get all books (Task 11: Using Promise)
-public_users.get('/', function (req, res) {
-    const getBooks = new Promise((resolve, reject) => {
-        resolve(books);
-    });
-    getBooks.then((bookList) => {
-        res.status(200).send(JSON.stringify(bookList, null, 4));
-    });
-});
-
-// Task 11: Get book details based on ISBN (Using Promise)
-public_users.get('/isbn/:isbn', function (req, res) {
-    const isbn = req.params.isbn;
-    new Promise((resolve, reject) => {
-        if (books[isbn]) resolve(books[isbn]);
-        else reject("Book not found");
-    })
-    .then(book => res.status(200).json(book))
-    .catch(err => res.status(404).json({message: err}));
-});
-
-// Task 12: Get book details based on author (Using Promise)
-public_users.get('/author/:author', function (req, res) {
-    const author = req.params.author;
-    new Promise((resolve) => {
-        let filtered = Object.values(books).filter(b => b.author === author);
-        resolve(filtered);
-    })
-    .then(result => res.status(200).json(result));
-});
-
-// Task 13: Get all books based on title (Using Promise)
-public_users.get('/title/:title', function (req, res) {
-    const title = req.params.title;
-    new Promise((resolve) => {
-        let filtered = Object.values(books).filter(b => b.title === title);
-        resolve(filtered);
-    })
-    .then(result => res.status(200).json(result));
-});
-
-// Task 6: Get book review
-public_users.get('/review/:isbn', function (req, res) {
-    const isbn = req.params.isbn;
-    if (books[isbn]) {
-        res.status(200).json(books[isbn].reviews);
-    } else {
-        res.status(404).json({message: "No ISBN found"});
+// Register a user
+public_users.post("/register", (req,res) => {
+  const { username, password } = req.body;
+  if (username && password) {
+    if (!isValid(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registered. Now you can login"});
     }
+    return res.status(404).json({message: "User already exists!"});
+  } 
+  return res.status(404).json({message: "Unable to register user."});
+});
+
+// Task 10: Get the book list available in the shop using Promises
+public_users.get('/', function (req, res) {
+  const getBooks = new Promise((resolve) => {
+    resolve(books);
+  });
+  getBooks.then((bookList) => res.send(JSON.stringify(bookList, null, 4)));
+});
+
+// Task 11: Get book details based on ISBN using Promises
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  new Promise((resolve, reject) => {
+    if (books[isbn]) resolve(books[isbn]);
+    else reject("Book not found");
+  })
+  .then(book => res.send(JSON.stringify(book, null, 4)))
+  .catch(err => res.status(404).send(err));
+});
+
+// Task 12: Get book details based on author using Promises
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  new Promise((resolve) => {
+    let filtered = Object.values(books).filter(b => b.author === author);
+    resolve(filtered);
+  })
+  .then(result => res.send(JSON.stringify(result, null, 4)));
+});
+
+// Task 13: Get all books based on title using Promises
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title;
+  new Promise((resolve) => {
+    let filtered = Object.values(books).filter(b => b.title === title);
+    resolve(filtered);
+  })
+  .then(result => res.send(JSON.stringify(result, null, 4)));
+});
+
+// Get book review
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    res.send(JSON.stringify(books[isbn].reviews, null, 4));
+  } else {
+    res.status(404).json({message: "ISBN not found"});
+  }
 });
 
 module.exports.general = public_users;
